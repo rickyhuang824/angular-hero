403931f71ca7444f75a3be5e7d5fb13a9770383c
rickyhuang824 <rickyhuang0824@gmail.com>
2017-10-22 18:03:12 -0700

    add text box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+
+export class Hero {
+  id: number;
+  name: string;
+
+}
+
 @Component({
+
+  template: `
+          <h1>{{title}}</h1>
+          <h2>{{hero.name}} details!</h2>
+          <div><label>id: </label>{{hero.id}} </div>
+          <div><label>name: </label>
+          <input [(ngModel)] = "hero.name" placeholder = "name">
+          </div>
+             `,
   selector: 'app-root',
-  templateUrl: './app.component.html',
+ // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heros';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+
+  };
 }
+
+
+
